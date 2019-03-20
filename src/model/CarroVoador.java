9eca528ea12fc888b8e39ce5af684e050e15d6f9
profthiagosouza <thiagoein@gmail.com>
2019-03-19 21:28:34 -0300

    Herança com classes abstratas

---
+++ src/model/CarroVoador.java
@@ -0,0 +1,12 @@
+package model;
+
+public class CarroVoador extends Carro {
+
+	public String acelerar() {
+		return "Acelerando o carro voador";
+	}
+	
+	public String frear() {
+		return "Freando o carro";
+	}
+}
---
+++ src/model/Veiculo.java
@@ -0,0 +1,12 @@
+package model;
+
+public abstract class Veiculo {
+	
+	//private final double constante = 3.0;
+	
+	//private int numeroRodas;
+	
+	public abstract String acelerar();
+	
+	public abstract String frear();
+}
